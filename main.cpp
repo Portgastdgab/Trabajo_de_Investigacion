4d197e91f076b9924e8de6d20beaec6f1be85b57
GoldGab <gabriel.choque@ucsp.edu.pe>
2021-05-30 17:31:01 -0500

    Inicio Trabajo de Investigacion

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int Euclides_clásico() {
+
+}
+
+int Euclides_con_menor_resto() {
+
+}
+
+int binario_del_mcd() {
+
+}
+
+int Lehmer_del_mcd() {
+
+}
+
+int Otro_algoritmo_que_sugiera() {
+
+}
+
+int main() {
+//    Euclides_clásico();
+//    Euclides_con_menor_resto();
+//    binario_del_mcd();
+//    Lehmer_del_mcd();
+//    Otro_algoritmo_que_sugiera();
+}
