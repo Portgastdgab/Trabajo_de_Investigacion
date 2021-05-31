e1974eee0c11c03f74cefb64e63745d8edeca7fc
GoldGab <gabriel.choque@ucsp.edu.pe>
2021-05-30 19:01:34 -0500

    Euclides sin ntl

--- main.cpp
+++ main.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
+#include<stdlib.h>
+#include<time.h>
+#include <cmath>
 
-int Euclides_clásico() {
+using namespace std;
 
+int bits(int bits) {
+    int begin = pow(2, bits)/2, end = pow(2, bits)-1;
+    srand(time(NULL));
+    return begin + rand() % (end - begin);
 }
 
-int Euclides_con_menor_resto() {
-
+int module(int a, int n){
+    int r = a-n*(a/n);
+    r = r+(r<0)*n;
+    return r;
 }
 
-int binario_del_mcd() {
+void Euclides_clasico(int a, int b){
+    int temp_a = a;
+    int temp_b = b;
+    while (module(temp_a, temp_b) != 0){
+        int temp = temp_b;
+        temp_b = module(temp_a, temp_b);
+        temp_a = temp;
+    }
+    cout<<"mcd("<<a<<", "<<b<<") = "<<temp_b;
+}
+void Euclides_con_menor_resto(int a, int b){
+    cout<<"mcd("<<a<<", "<<b<<") = ";
 
 }
-
-int Lehmer_del_mcd() {
-
+void binario_del_mcd(int a, int b){
+    cout<<"mcd("<<a<<", "<<b<<") = ";
 }
-
-int Otro_algoritmo_que_sugiera() {
-
+void Lehmer_del_mcd(int a, int b){
+    cout<<"mcd("<<a<<", "<<b<<") = ";
 }
-
+void Otro_algoritmo_que_sugiera(int a, int b){
+    cout<<"mcd("<<a<<", "<<b<<") = ";
+}
 int main() {
-//    Euclides_clásico();
-//    Euclides_con_menor_resto();
-//    binario_del_mcd();
-//    Lehmer_del_mcd();
-//    Otro_algoritmo_que_sugiera();
+    int n_bits = 10;
+    int a = bits(n_bits);
+    int b = a*0.941852963;
+    cout<<a<<"     "<<b<<endl;
+    Euclides_clasico(a, b);
 }
